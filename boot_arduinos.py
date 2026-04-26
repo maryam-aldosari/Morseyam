e42387e306375983c92759de0dba214e7e709b1e
maryam-aldosari <tuv26828@temple.edu>
2026-04-26 17:13:47 -0400

    Add files via upload

---
+++ boot_arduinos.py
@@ -0,0 +1,38 @@
+
+#import esp
+#esp.osdebug(None)
+import webrepl
+webrepl.start()
+
+#import esp
+#esp.osdebug(None)
+import webrepl
+webrepl.start()
+
+import network
+import time
+import webrepl
+
+
+SSID = '905@Vantage'
+PASSWORD = 'X10rYHkhGzCI'
+
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('Connecting to WiFi...')
+        wlan.connect(SSID, PASSWORD)
+        while not wlan.isconnected():
+            time.sleep(1)
+            pass
+    
+
+    ip_address = wlan.ifconfig()[0]
+    print('Connected! Network config:', wlan.ifconfig())
+    print(f'Your ESP32 IP Address is: {ip_address}')
+
+connect_wifi()
+
+
+webrepl.start()
